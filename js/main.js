cdb11a0597d2540afcb192d27f52a85338264940
faradeycage <smeernova@gmail.com>
2021-06-06 00:15:19 +0300

    Исправила замечания по PR

--- js/main.js
+++ js/main.js
@@ -2,22 +2,20 @@
 //источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomIntInclusive(min, max) {
-  if (min > max || min <0) {
-    throw new Error('задан неверный диапазон');}
+  if (min > max || min < 0) {
+    throw new Error("задан неверный диапазон");
+  }
   min = Math.ceil(min); //округляет число вверх
   max = Math.floor(max); //округляет число вниз
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
-console.log(getRandomIntInclusive(5, 1));
-
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-function getRandomFloatInclusive(min, max, charactersNumber) {
-  if (min > max || min <0) {
-    throw new Error('задан неверный диапазон');}
-  let randomFloat = Math.random() * (max - min + 1) + min;
-  charactersCount = Math.pow(10, charactersNumber);
-  randomFloat = Math.floor(randomFloat * charactersCount) / charactersCount;
-  return randomFloat;
+function getRandomFloatInclusive(min, max, precision) {
+  if (min > max || min < 0) {
+    throw new Error("задан неверный диапазон");
+  }
+  const randomFloat = Math.random() * (max - min + 1) + min;
+  const multiplier = Math.pow(10, precision);
+  return Math.floor(randomFloat * multiplier) / multiplier;
 }
-console.log(getRandomFloatInclusive(1.1, 10.1, 3));
