e1f21d4ac6ea669f04eef7640f204782bc19d4c3
faradeycage <smeernova@gmail.com>
2021-06-06 00:24:24 +0300

    Исправила замечания

--- js/main.js
+++ js/main.js
@@ -3,19 +3,21 @@
 
 function getRandomIntInclusive(min, max) {
   if (min > max || min < 0) {
-    throw new Error("задан неверный диапазон");
+    throw new Error('задан неверный диапазон');
   }
   min = Math.ceil(min); //округляет число вверх
   max = Math.floor(max); //округляет число вниз
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
+getRandomIntInclusive(0,10);
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 function getRandomFloatInclusive(min, max, precision) {
   if (min > max || min < 0) {
-    throw new Error("задан неверный диапазон");
+    throw new Error('задан неверный диапазон');
   }
   const randomFloat = Math.random() * (max - min + 1) + min;
   const multiplier = Math.pow(10, precision);
   return Math.floor(randomFloat * multiplier) / multiplier;
 }
+getRandomFloatInclusive(0, 10, 3);
