e60f86c769431ebdefc0f293cf8ae0b77d618452
faradeycage <smeernova@gmail.com>
2021-06-11 23:56:04 +0300

    исправила замечания

--- js/main.js
+++ js/main.js
@@ -20,6 +20,15 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
+/* привет. для всех вот таких вещей
+const randomApartTypeIndex = getRandomIntInclusive(0, APART_TYPE.length - 1);
+APART_TYPE[randomApartTypeIndex]
+создай функцию, которая будет получать один случайны элемент из массива и работать как
+const getRandomItem = (items) => items[getRandomIntInclusive(0, items.length - 1)];
+и вызывай её как getRandomItem(APART_TYPE), getRandomItem(FEATURES) и т.д.
+ещё несколько мелких замечаний оставил */
+
+const getRandomItem = (items) => items[getRandomIntInclusive(0, items.length - 1)];
 
 const TITLES = [
   'Заголовок 1',
@@ -34,13 +43,13 @@ const TITLES = [
   'Заголовок 10',
 ];
 
-const APART_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const APPARTS_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
-const CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
+const CHECK_IN_OUT_TIMES = ['12:00', '13:00', '14:00'];
 
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Описание 1',
   'Описание 2',
   'Описание 3',
@@ -65,32 +74,39 @@ const createSimilarAd = () => {
     userAvatarCount = `0${userAvatarCount}`;
   }
   //title, строка — заголовок предложения. Придумайте самостоятельно.
-  const randomTitleIndex = getRandomIntInclusive(0, TITLES.length - 1);
+  const randomTitleIndex = getRandomItem(TITLES);
 
   //price, число — стоимость. Случайное целое положительное число.
   const randomPrice = getRandomIntInclusive(1000, 100000);
 
   //type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-  const randomApartTypeIndex = getRandomIntInclusive(0, APART_TYPE.length - 1);
+  const randomApartTypeIndex = getRandomItem(APPARTS_TYPE);
 
   //address, строка — адрес предложения.
   //Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
   //location, объект — местоположение в виде географических координат. Состоит из двух полей:
   //lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
   //lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
-  const latitude = getRandomFloatInclusive(35.65000, 35.70000, 5);
-  const longitude = getRandomFloatInclusive(139.70000, 139.80000, 5);
+  const MIN_LNG = 139.70000;
+  const MAX_LNG = 139.80000;
+  const MIN_LNT = 35.65000;
+  const MAX_LNT = 35.70000;
+
+  const latitude = getRandomFloatInclusive(MIN_LNT, MAX_LNT, 5);
+  const longitude = getRandomFloatInclusive(MIN_LNG, MAX_LNG, 5);
 
   //rooms, число — количество комнат. Случайное целое положительное число.
-  const roomsCount = getRandomIntInclusive(1, 10);
+  const MAX_ROOMS_COUNT = 10;
+  const roomsCount = getRandomIntInclusive(1, MAX_ROOMS_COUNT);
 
   //guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-  const guestsCount = getRandomIntInclusive(1, 10);
+  const MAX_GUESTS_COUNT = 10;
+  const guestsCount = getRandomIntInclusive(1, MAX_GUESTS_COUNT);
 
   //checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
   //checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-  const randomCheckInIndex = getRandomIntInclusive(0, CHECK_IN_OUT.length - 1);
-  const randomCheckOutIndex = getRandomIntInclusive(0, CHECK_IN_OUT.length - 1);
+  const randomCheckInIndex = getRandomIntInclusive(0, CHECK_IN_OUT_TIMES.length - 1);
+  const randomCheckOutIndex = getRandomIntInclusive(0, CHECK_IN_OUT_TIMES.length - 1);
 
   //features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner.
   //Значения не должны повторяться.
@@ -113,7 +129,7 @@ const createSimilarAd = () => {
   }
 
   //description, строка — описание помещения. Придумайте самостоятельно.
-  const randomDescriptionIndex = getRandomIntInclusive(1, DESCRIPTION.length - 1);
+  const randomDescriptionIndex = getRandomIntInclusive(1, DESCRIPTIONS.length - 1);
 
   //photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
   const photosArrayLength = getRandomIntInclusive(1, 10);
@@ -125,20 +141,20 @@ const createSimilarAd = () => {
 
   return {
     author: {
-      avatar: `img/avatars/${  userAvatarCount  }.png`,
+      avatar: `img/avatars/${userAvatarCount}.png`,
     },
 
     offer: {
-      title: TITLES[randomTitleIndex],
-      address: `${latitude  }, ${  longitude}`,
+      title: getRandomItem(TITLES),
+      address: `${latitude}, ${longitude}`,
       price: randomPrice,
-      type: APART_TYPE[randomApartTypeIndex],
+      type: getRandomItem(APPARTS_TYPE),
       rooms: roomsCount,
       guests: guestsCount,
-      checkin: CHECK_IN_OUT[randomCheckInIndex],
-      checkout: CHECK_IN_OUT[randomCheckOutIndex],
+      checkin: getRandomItem(CHECK_IN_OUT_TIMES),
+      checkout: getRandomItem(CHECK_IN_OUT_TIMES),
       features: unique(),
-      description: DESCRIPTION[randomDescriptionIndex],
+      description: getRandomItem(DESCRIPTIONS),
       photos: photosGenerated,
     },
 
@@ -153,3 +169,5 @@ const createSimilarAd = () => {
 const SIMILAR_AD_COUNT = 10;
 // eslint-disable-next-line no-unused-vars
 const similarAds = new Array(SIMILAR_AD_COUNT).fill(null).map(() => createSimilarAd());
+
+console.log(JSON.stringify(similarAds));
