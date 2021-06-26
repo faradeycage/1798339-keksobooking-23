790e5b247bde5ff1113b10caacaf4fad7959f424
faradeycage <smeernova@gmail.com>
2021-06-27 00:17:29 +0300

    передала нулевую рекламу в функцию

--- js/main.js
+++ js/main.js
@@ -5,5 +5,5 @@ import {renderAds} from './renderAds.js';
 // eslint-disable-next-line no-unused-vars
 const similarAds = getAds();
 // eslint-disable-next-line no-unused-vars
-const renderedAds = renderAds();
+const renderedAds = renderAds(similarAds[0]);
 
--- js/renderAds.js
+++ js/renderAds.js
@@ -1,14 +1,10 @@
-import {
-  getAds
-} from './data.js';
-const info = getAds();
 
-const renderAds = () => {
+const renderAds = (info) => {
   //Выведите заголовок объявления offer.title в заголовок .popup__title.
   const template = document.querySelector('#card').content;
   const titleTemplate = template.querySelector('.popup__title');
   const title = titleTemplate.cloneNode(true);
-  title.textContent = info[1].offer.title;
+  title.textContent = info.offer.title;
   //Отрисуйте один из сгенерированных DOM-элементов, например первый, в блок #map-canvas, чтобы проверить, что данные в разметку были вставлены корректно.
   const mapCanvas = document.querySelector('.map__canvas');
   if (title.textContent === '') {
@@ -19,7 +15,7 @@ const renderAds = () => {
   //Выведите адрес offer.address в блок .popup__text--address.
   const addressTextTemplate = template.querySelector('.popup__text--address');
   const addressText = addressTextTemplate.cloneNode(true);
-  addressText.textContent = info[1].offer.address;
+  addressText.textContent = info.offer.address;
   if (addressText.textContent === '') {
     addressText.classList.add('hidden');
   }
@@ -28,8 +24,8 @@ const renderAds = () => {
   //Выведите цену offer.price в блок .popup__text--price строкой вида {{offer.price}} ₽/ночь. Например, «5200 ₽/ночь».
   const priceTemplate = template.querySelector('.popup__text--price');
   const price = priceTemplate.cloneNode(true);
-  price.textContent = `${info[1].offer.price  } ₽/ночь`;
-  if (info[1].offer.price === '') {
+  price.textContent = `${info.offer.price  } ₽/ночь`;
+  if (info.offer.price === '') {
     price.classList.add('hidden');
   }
   mapCanvas.appendChild(price);
@@ -44,23 +40,23 @@ const renderAds = () => {
   const typeTemplate = template.querySelector('.popup__type');
   const type = typeTemplate.cloneNode(true);
   let typeInRussian = '';
-  if (info[1].offer.type === 'palace') {
+  if (info.offer.type === 'palace') {
     typeInRussian = 'Дворец';
   }
-  if (info[1].offer.type === 'flat') {
+  if (info.offer.type === 'flat') {
     typeInRussian = 'Квартира';
   }
-  if (info[1].offer.type === 'house') {
+  if (info.offer.type === 'house') {
     typeInRussian = 'Дом';
   }
-  if (info[1].offer.type === 'bungalow') {
+  if (info.offer.type === 'bungalow') {
     typeInRussian = 'Бунгало';
   }
-  if (info[1].offer.type === 'hotel') {
+  if (info.offer.type === 'hotel') {
     typeInRussian = 'Отель';
   }
   type.textContent = typeInRussian;
-  if (info[1].offer.type === '') {
+  if (info.offer.type === '') {
     type.classList.add('hidden');
   }
   mapCanvas.appendChild(type);
@@ -69,8 +65,8 @@ const renderAds = () => {
   //строкой вида {{offer.rooms}} комнаты для {{offer.guests}} гостей. Например, «2 комнаты для 3 гостей».
   const roomsAndGuestsTemplate = template.querySelector('.popup__text--capacity');
   const roomsAndGuests = roomsAndGuestsTemplate.cloneNode(true);
-  roomsAndGuests.textContent = `${info[1].offer.rooms  } комнаты для ${  info[1].offer.guests} гостей`;
-  if (info[1].offer.rooms === '' && info[1].offer.guests === '') {
+  roomsAndGuests.textContent = `${info.offer.rooms  } комнаты для ${  info.offer.guests} гостей`;
+  if (info.offer.rooms === '' && info.offer.guests === '') {
     roomsAndGuests.classList.add('hidden');
   }
   mapCanvas.appendChild(roomsAndGuests);
@@ -79,8 +75,8 @@ const renderAds = () => {
   //Заезд после {{offer.checkin}}, выезд до {{offer.checkout}}. Например, «Заезд после 14:00, выезд до 14:00».
   const checkinAndCheckoutTemplate = template.querySelector('.popup__text--time');
   const checkinAndCheckout = checkinAndCheckoutTemplate.cloneNode(true);
-  checkinAndCheckout.textContent = `Заезд после ${info[1].offer.checkin}, выезд до ${info[1].offer.checkout}`;
-  if (info[1].offer.checkin === '' && info[1].offer.checkout === '') {
+  checkinAndCheckout.textContent = `Заезд после ${info.offer.checkin}, выезд до ${info.offer.checkout}`;
+  if (info.offer.checkin === '' && info.offer.checkout === '') {
     checkinAndCheckout.classList.add('hidden');
   }
   mapCanvas.appendChild(checkinAndCheckout);
@@ -88,7 +84,7 @@ const renderAds = () => {
   //В список .popup__features выведите все доступные удобства в объявлении.
   const featuresTemplate = template.querySelector('.popup__features');
   const features = featuresTemplate.cloneNode(true);
-  features.textContent = info[1].offer.features;
+  features.textContent = info.offer.features;
   if (features.textContent === '') {
     features.classList.add('hidden');
   }
@@ -97,7 +93,7 @@ const renderAds = () => {
   //В блок .popup__description выведите описание объекта недвижимости offer.description
   const descriptionTemplate = template.querySelector('.popup__description');
   const description = descriptionTemplate.cloneNode(true);
-  description.textContent = info[1].offer.description;
+  description.textContent = info.offer.description;
   if (description.textContent === '') {
     description.classList.add('hidden');
   }
@@ -108,10 +104,10 @@ const renderAds = () => {
   const photosTemplate = template.querySelector('.popup__photos');
   const photos = photosTemplate.cloneNode(false);
 
-  for (let idx = 0; idx < info[1].offer.photos.length; idx++) {
+  for (let idx = 0; idx < info.offer.photos.length; idx++) {
     const photoTpl = template.querySelector('.popup__photo');
     const photo = photoTpl.cloneNode(true);
-    photo.src = info[1].offer.photos[idx];
+    photo.src = info.offer.photos[idx];
     photos.appendChild(photo);
   }
   if (photos === '') {
@@ -123,8 +119,8 @@ const renderAds = () => {
   //на значение поля author.avatar.
   const avatarTemplate = template.querySelector('.popup__avatar');
   const avatar = avatarTemplate.cloneNode(true);
-  avatar.src = info[1].author.avatar;
-  if (info[1].author.avatar === '') {
+  avatar.src = info.author.avatar;
+  if (info.author.avatar === '') {
     avatar.classList.add('hidden');
   }
   mapCanvas.appendChild(avatar);
