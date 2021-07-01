25e03d30eb7c02d96a469b653e0ac935146ea0bf
faradeycage <smeernova@gmail.com>
2021-07-01 16:54:53 +0300

    обернула в функцию

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {getAds} from './data.js';
 //import {renderAd} from './renderAd.js';
 import {adFormValidate, formPageDisable, formPageActivate} from './adForm.js';
+import {showMap} from './map.js';
 
 
 // eslint-disable-next-line no-unused-vars
@@ -9,3 +10,4 @@ const similarAds = getAds();
 adFormValidate();
 formPageDisable();
 formPageActivate();
+showMap();
--- js/map.js
+++ js/map.js
@@ -10,70 +10,73 @@ import {
   renderAd
 } from './renderAd.js';
 
+const showMap = () => {
+  const map = L.map('map-canvas')
+    .on('load', () => {
+      formPageActivate();
+    })
+    .setView({
+      lat: 35.6895,
+      lng: 139.692,
+    }, 8);
 
-const map = L.map('map-canvas')
-  .on('load', () => {
-    formPageActivate();
-  })
-  .setView({
-    lat: 35.6895,
-    lng: 139.692,
-  }, 8);
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
 
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
-
-const mainPinIcon = L.icon({
-  iconUrl: 'img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
-
-const mainPinMarker = L.marker({
-  lat: 35.6895,
-  lng: 139.692,
-}, {
-  draggable: true,
-  icon: mainPinIcon,
-});
-mainPinMarker.addTo(map);
-
-mainPinMarker.on('moveend', (evt) => {
-  const address = document.querySelector('#address');
-  const lat = evt.target.getLatLng().lat.toFixed(5);
-  const lng = evt.target.getLatLng().lng.toFixed(5);
-  address.value = `${lat  }, ${  lng}`;
-});
-
-const adPinIcon = L.icon({
-  iconUrl: 'img/pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
-
-const createCustomPopup = (point) => {
-  const balloonTemplate = document.querySelector('#card').content.querySelector('.popup');
-  let popupElement = balloonTemplate.cloneNode(true);
-  popupElement = renderAd(point);
-  return popupElement;
-};
-
-const ads = getAds();
-for (const ad of ads) {
-  const adPinMarker = L.marker({
-    lat: ad.location.lat,
-    lng: ad.location.lng,
-  }, {
-    icon: adPinIcon,
-  }, {
-    keepInView: true,
+  const mainPinIcon = L.icon({
+    iconUrl: 'img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
   });
 
-  adPinMarker
-    .addTo(map)
-    .bindPopup(() => createCustomPopup(ad));
+  const mainPinMarker = L.marker({
+    lat: 35.6895,
+    lng: 139.692,
+  }, {
+    draggable: true,
+    icon: mainPinIcon,
+  });
+  mainPinMarker.addTo(map);
+
+  mainPinMarker.on('moveend', (evt) => {
+    const address = document.querySelector('#address');
+    const lat = evt.target.getLatLng().lat.toFixed(5);
+    const lng = evt.target.getLatLng().lng.toFixed(5);
+    address.value = `${lat  }, ${  lng}`;
+  });
+
+  const adPinIcon = L.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
+  });
+
+  const createCustomPopup = (point) => {
+    const balloonTemplate = document.querySelector('#card').content.querySelector('.popup');
+    let popupElement = balloonTemplate.cloneNode(true);
+    popupElement = renderAd(point);
+    return popupElement;
+  };
+
+  const ads = getAds();
+  for (const ad of ads) {
+    const adPinMarker = L.marker({
+      lat: ad.location.lat,
+      lng: ad.location.lng,
+    }, {
+      icon: adPinIcon,
+    }, {
+      keepInView: true,
+    });
+
+    adPinMarker
+      .addTo(map)
+      .bindPopup(() => createCustomPopup(ad));
+  }
 }
 
+export
+{showMap};
